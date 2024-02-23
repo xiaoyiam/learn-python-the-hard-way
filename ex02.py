bdf2ffcf007bd57ef56f427e97314fe9c177b8c1
xiaoyi <xiaoyiam@gmail.com>
2024-02-23 23:36:00 +0800

    add ex02

---
+++ ex02.py
@@ -0,0 +1,9 @@
+# A comment , this is so you can read yout program later,
+# Anything after the # is ingored by python.
+
+print("I could have code like this.") # and the comment after is ingored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
